53fe0793190d65aa450216c2fd9c18126a066c29
MostafaSakr <mostafa.sakr4@gmail.com>
2018-01-23 04:19:13 +0100

    #7 test sample

--- HotelR.Test/UnitTest1.cs
+++ HotelR.Test/UnitTest1.cs
@@ -2,11 +2,49 @@ using System;
 
 namespace HotelR.Test
 {
-    public class UnitTest1
+    [TestFixture]
+    public class BoReservationTest
     {
-        public void Test1()
+        private Mock<IUnitOfWork> unitOfWorkMocked;
+        [SetUp]
+        public void Setup()
         {
+            unitOfWorkMocked = new Mock<IUnitOfWork>();
+            UnityConfiguration.Container.RegisterInstance(typeof(IUnitOfWork), "", unitOfWorkMocked.Object, new PerThreadLifetimeManager());
+        }
 
+        [Test]
+        public void BookReservation_Test()
+        {
+            //Arrange
+            unitOfWorkMocked.Setup(x => x.RepositoryFor<DtoReservation>().Insert(It.IsAny<DtoReservation>())).Returns<DtoReservation>(x => x);
+            var boReservation = new BoReservation();
+
+            var dt = DateTime.Now;
+            var room = new DtoRoom()
+            {
+                CreatedOn = dt,
+                Id = Guid.NewGuid(),
+                ModifiedOn = dt,
+                Number = "1",
+                RoomType = new DtoRoomType()
+                {
+                    CreatedOn = dt,
+                    Id = Guid.NewGuid(),
+                    ModifiedOn = dt,
+                    CancellationFeeNightsCount = 1,
+                    DepositFeePercentage = 70,
+                    Rate = 100
+                }
+            };
+
+            //Act
+            var dtoReservation = boReservation.Book(new DtoGuest(), room, DateTime.Now,
+                DateTime.Now.AddDays(1));
+
+            //Assert
+            Assert.IsTrue(dtoReservation.ReservationStatusList.Select(x => x.ReservationStatus).Contains(ReservationStatus.Booked));
         }
     }
+
 }
